75d8a032508e336245f4f9ae13d6ea8d0b8a7db4
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2014-12-31 15:37:21 +0000

    sending html back to server

--- app/client/main.js
+++ app/client/main.js
@@ -1,8 +1,34 @@
 var ipc = require('ipc');
-(function(){
-    ipc.on('render', function(data){
-        console.log('peng');
-        console.log(data);
-    });
-})();
 
+var currentDocument;
+var pages = document.getElementById('pages');
+var articles = document.getElementById('articles');
+var documentlist = document.getElementById('documentlist');
+var editor = document.getElementById('editor');
+var mediumEditor = new MediumEditor(editor);
+
+var onDocumentClick = function(){
+    if(this.path !== currentDocument){
+        editor.innerHTML = this.body;
+        currentDocument = this.path;
+    }
+};
+
+editor.addEventListener('input', function(){
+    ipc.send('autosave', {path: currentDocument, content: editor.innerHTML});
+});
+
+var renderList = function(element, list){
+    list.forEach(function(item){
+        var title = item.attributes.title || 'Untitled';
+        var li = document.createElement('li');
+        li.innerHTML = '' + item.basename + ' - ' + title;
+        li.addEventListener('click', onDocumentClick.bind(item));
+        element.appendChild(li);
+    });
+};
+
+ipc.on('render', function(data){
+    renderList(articles, data.articles);
+    renderList(pages, data.pages);
+});
